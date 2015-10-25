833df9bb6cc6b0e7fd4c36aced2eaffa88e15539
Ross <dyson.ross@gmail.com>
2015-10-25 12:50:06 -0700

    init commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Empty JS for your own code to be here
+$(document).ready(function () {
+    $('.demoImgs').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false
+    });
+    
+    $('.demoTxt').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false
+    });
+});
